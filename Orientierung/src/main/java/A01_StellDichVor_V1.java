12e79429b2c0d3ecf9e04d13861f7aec028ce760
AbdurakhmanV <146175665+AbdurakhmanV@users.noreply.github.com>
2024-01-19 09:00:19 +0100

    Ich stelle mich vor. I introduce myself.

---
+++ Orientierung/src/main/java/A01_StellDichVor_V1.java
@@ -0,0 +1,53 @@
+public class A01_StellDichVor_V1 {
+    public static void main(String[] args) {
+        String name = "Abdurakhman";
+        int age = 26;
+        String town = "Linz";
+        char lastnameInitial = 'V';
+        double salary = 30;
+        boolean request = true;
+        System.out.println("Mein Name ist " + name + ".");
+        System.out.println("Ich bin " + age + " Jahre alt und wohne derzeit in " + town + ".");
+        System.out.println("Der letzte Buchstabe meines Nachnamens ist " + lastnameInitial + ".");
+        System.out.println("Nach der Ausbildung werde ich " + salary + " EURO/h verdienen, und glaube heute an mich: " + request + ".");
+    }
+}
+
+
+//Im folgenden Text sind einige Wörter und Zahlen in Großbuchstaben/in gelb geschrieben. Diese sollst du durch Variablen mit passenden Datentypen ersetzen.
+//
+//        Verwende für jeden gelben Part eine Variable, und gebe jeweils die ganzen Sätze mit System.out.println() aus!
+//
+//        Mein Vorname ist Alexander.
+//        Ich bin 28 Jahre alt und wohne derzeit in Kirchdorf.
+//        Der letzte Buchstabe meines Nachnamens ist S.
+//        Nach der Ausbildung werde ich 50.0 EURO/h verdienen, und glaube heute an mich: true.
+//
+//        Anforderung 1: Verwende jeden unserer Basis-Datentypen (int, double, char, String, boolean) mindestens 1x, um deine Variablen zu erstellen. Alle Variablen stehen am Anfang deines Programms gesammelt, so dass man sie einfach abändern kann.
+//
+//        Anforderung 2: Wenn du die Variablen abänderst, ändert sich automatisch die Ausgabe entsprechend.
+//
+//        Anforderung 3: Erzähle uns von dir! Ersetze die Werte oben mit denen, die auf dich zutreffen!
+
+
+//    Wir programmieren einen einfachen Taschenrechner!
+//
+//        Lege 2 int-Variablen an und gebe ihnen Startwerte deiner Wahl, also z.B.:
+//
+//        a = 5
+//        b = 3
+//
+//        Dann gib mit System.out.println() Zeile für Zeile aus:
+//
+//        Erste Variable: 5
+//        Zweite Variable: 3
+//        5 + 3 = 8
+//        5 – 3 = 2
+//        5 * 3 = 15
+//        5 / 3 = 1,666666667
+//        5 % 3 = 2
+//
+//        Anforderung 1: Dein Programm muss sowohl die beiden Variablen, die Rechnungen als auch die Ergebnisse ausgeben.
+//
+//        Anforderung 2: Änderst du eine oder beide der Variablen a und/oder b, müssen die Rechnungen, die daraus generiert werden, sich entsprechend mit ändern.
+
